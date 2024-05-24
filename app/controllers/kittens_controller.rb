6f6dcef8c4b9d4d40f797a393cfb4c6550a31638
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-05-23 21:33:47 -0700

    Add form for creating kitten

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -4,7 +4,27 @@ class KittensController < ApplicationController
 		@kittens = Kitten.all
 	end
 
+	def new
+		@kitten = Kitten.new
+	end
+
+	def create
+		@kitten = Kitten.new(kitten_params)
+
+		if @kitten.save
+			redirect_to @kitten
+		else
+			render :new, status: :unprocessable_entity
+		end
+	end
+
 	def show
-		@kitten = Kitten.find[params(:id)]
+		@kitten = Kitten.find(params[:id])
+	end
+
+	private
+
+	def kitten_params
+		params.require(:kitten).permit(:name, :age, :cuteness, :softness)
 	end
 end
