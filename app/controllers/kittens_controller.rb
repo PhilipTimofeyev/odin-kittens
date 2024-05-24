fda994c14c6294fdb9f948ea2e15c42a8c83de8f
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2024-05-23 21:38:25 -0700

    Add update and edit function to controller

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -18,8 +18,22 @@ class KittensController < ApplicationController
 		end
 	end
 
+	def edit
+		@kitten = Kitten.find(params[:id]) 
+	end
+
 	def show
 		@kitten = Kitten.find(params[:id])
+	end
+
+	def update
+		@kitten = Kitten.find(params[:id]) 
+
+		if @kitten.update(kitten_params)
+			redirect_to @kitten
+		else
+			render :new, status: :unprocessable_entity
+		end
 	end
 
 	private
